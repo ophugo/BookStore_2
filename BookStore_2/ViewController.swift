622629469284cd61900663663b385821ba310864
Hugo <ophugo@hotmail.com>
2020-03-31 18:27:07 -0700

    Initial Commit

---
+++ BookStore_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookStore_2
+//
+//  Created by Hugo A Valencia on 3/31/20.
+//  Copyright © 2020 Hugo A Valencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
